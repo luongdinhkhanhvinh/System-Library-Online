6d042041c941d4a6df7be25da610d7134e2f2725
luongdinhkhanhvinh <luongdkvinh@gmail.com>
2019-12-22 21:33:32 +0700

    second commit

--- SQL/library.sql
+++ SQL/library.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
--- http://www.phpmyadmin.net
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jul 17, 2017 at 06:15 PM
--- Server version: 5.6.26
--- PHP Version: 5.5.28
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th12 22, 2019 lúc 03:31 PM
+-- Phiên bản máy phục vụ: 10.4.8-MariaDB
+-- Phiên bản PHP: 7.3.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,75 +19,75 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `library`
+-- Cơ sở dữ liệu: `library`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Cấu trúc bảng cho bảng `admin`
 --
 
-CREATE TABLE IF NOT EXISTS `admin` (
+CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
   `FullName` varchar(100) DEFAULT NULL,
   `AdminEmail` varchar(120) DEFAULT NULL,
   `UserName` varchar(100) NOT NULL,
   `Password` varchar(100) NOT NULL,
-  `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `admin`
+-- Đang đổ dữ liệu cho bảng `admin`
 --
 
 INSERT INTO `admin` (`id`, `FullName`, `AdminEmail`, `UserName`, `Password`, `updationDate`) VALUES
-(1, 'Anuj Kumar', 'phpgurukulofficial@gmail.com', 'admin', 'f925916e2754e5e03f75dd58a5733251', '2017-07-16 18:11:42');
+(1, 'Khanh Vinh', 'khanhvinh15@gmail.com', 'admin', 'admin', '2019-12-21 18:31:27');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblauthors`
+-- Cấu trúc bảng cho bảng `tblauthors`
 --
 
-CREATE TABLE IF NOT EXISTS `tblauthors` (
+CREATE TABLE `tblauthors` (
   `id` int(11) NOT NULL,
-  `AuthorName` varchar(159) DEFAULT NULL,
-  `creationDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+  `AuthorName` varchar(159) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `creationDate` timestamp NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tblauthors`
+-- Đang đổ dữ liệu cho bảng `tblauthors`
 --
 
 INSERT INTO `tblauthors` (`id`, `AuthorName`, `creationDate`, `UpdationDate`) VALUES
-(1, 'Anuj kumar', '2017-07-08 12:49:09', '2017-07-08 15:16:59'),
-(2, 'Chetan Bhagatt', '2017-07-08 14:30:23', '2017-07-08 15:15:09'),
-(3, 'Anita Desai', '2017-07-08 14:35:08', NULL),
-(4, 'HC Verma', '2017-07-08 14:35:21', NULL),
-(5, 'R.D. Sharma ', '2017-07-08 14:35:36', NULL),
-(9, 'fwdfrwer', '2017-07-08 15:22:03', NULL);
+(1, 'Khanh Vinh', '2017-07-08 12:49:09', '2019-12-21 18:31:43'),
+(2, 'Thanh Tuan', '2017-07-08 14:30:23', '2019-12-21 18:31:54'),
+(3, 'Quoc Viet', '2017-07-08 14:35:08', NULL),
+(4, 'Quy Trong', '2017-07-08 14:35:21', NULL),
+(5, 'Xuan Tung', '2017-07-08 14:35:36', NULL),
+(9, 'Thanh Vo', '2017-07-08 15:22:03', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblbooks`
+-- Cấu trúc bảng cho bảng `tblbooks`
 --
 
-CREATE TABLE IF NOT EXISTS `tblbooks` (
+CREATE TABLE `tblbooks` (
   `id` int(11) NOT NULL,
   `BookName` varchar(255) DEFAULT NULL,
   `CatId` int(11) DEFAULT NULL,
   `AuthorId` int(11) DEFAULT NULL,
   `ISBNNumber` int(11) DEFAULT NULL,
   `BookPrice` int(11) DEFAULT NULL,
-  `RegDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `RegDate` timestamp NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tblbooks`
+-- Đang đổ dữ liệu cho bảng `tblbooks`
 --
 
 INSERT INTO `tblbooks` (`id`, `BookName`, `CatId`, `AuthorId`, `ISBNNumber`, `BookPrice`, `RegDate`, `UpdationDate`) VALUES
@@ -95,19 +97,19 @@ INSERT INTO `tblbooks` (`id`, `BookName`, `CatId`, `AuthorId`, `ISBNNumber`, `Bo
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblcategory`
+-- Cấu trúc bảng cho bảng `tblcategory`
 --
 
-CREATE TABLE IF NOT EXISTS `tblcategory` (
+CREATE TABLE `tblcategory` (
   `id` int(11) NOT NULL,
   `CategoryName` varchar(150) DEFAULT NULL,
   `Status` int(1) DEFAULT NULL,
-  `CreationDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `UpdationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+  `CreationDate` timestamp NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tblcategory`
+-- Đang đổ dữ liệu cho bảng `tblcategory`
 --
 
 INSERT INTO `tblcategory` (`id`, `CategoryName`, `Status`, `CreationDate`, `UpdationDate`) VALUES
@@ -119,21 +121,21 @@ INSERT INTO `tblcategory` (`id`, `CategoryName`, `Status`, `CreationDate`, `Upda
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblissuedbookdetails`
+-- Cấu trúc bảng cho bảng `tblissuedbookdetails`
 --
 
-CREATE TABLE IF NOT EXISTS `tblissuedbookdetails` (
+CREATE TABLE `tblissuedbookdetails` (
   `id` int(11) NOT NULL,
   `BookId` int(11) DEFAULT NULL,
   `StudentID` varchar(150) DEFAULT NULL,
-  `IssuesDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `ReturnDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `IssuesDate` timestamp NULL DEFAULT current_timestamp(),
+  `ReturnDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
   `RetrunStatus` int(1) NOT NULL,
   `fine` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tblissuedbookdetails`
+-- Đang đổ dữ liệu cho bảng `tblissuedbookdetails`
 --
 
 INSERT INTO `tblissuedbookdetails` (`id`, `BookId`, `StudentID`, `IssuesDate`, `ReturnDate`, `RetrunStatus`, `fine`) VALUES
@@ -142,112 +144,121 @@ INSERT INTO `tblissuedbookdetails` (`id`, `BookId`, `StudentID`, `IssuesDate`, `
 (3, 3, 'SID002', '2017-07-15 06:13:40', NULL, 0, NULL),
 (4, 3, 'SID002', '2017-07-15 06:23:23', '2017-07-15 11:22:29', 1, 2),
 (5, 1, 'SID009', '2017-07-15 10:59:26', NULL, 0, NULL),
-(6, 3, 'SID011', '2017-07-15 18:02:55', NULL, 0, NULL);
+(6, 3, 'SID011', '2017-07-15 18:02:55', NULL, 0, NULL),
+(7, 1, '3', '2019-12-21 18:42:29', '2019-12-21 18:43:33', 1, 600);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblstudents`
+-- Cấu trúc bảng cho bảng `tblstudents`
 --
 
-CREATE TABLE IF NOT EXISTS `tblstudents` (
+CREATE TABLE `tblstudents` (
   `id` int(11) NOT NULL,
   `StudentId` varchar(100) DEFAULT NULL,
-  `FullName` varchar(120) DEFAULT NULL,
+  `FullName` varchar(120) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `EmailId` varchar(120) DEFAULT NULL,
   `MobileNumber` char(11) DEFAULT NULL,
   `Password` varchar(120) DEFAULT NULL,
   `Status` int(1) DEFAULT NULL,
-  `RegDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+  `RegDate` timestamp NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tblstudents`
+-- Đang đổ dữ liệu cho bảng `tblstudents`
 --
 
 INSERT INTO `tblstudents` (`id`, `StudentId`, `FullName`, `EmailId`, `MobileNumber`, `Password`, `Status`, `RegDate`, `UpdationDate`) VALUES
-(1, 'SID002', 'Anuj kumar', 'anuj.lpu1@gmail.com', '9865472555', 'f925916e2754e5e03f75dd58a5733251', 1, '2017-07-11 15:37:05', '2017-07-15 18:26:21'),
-(4, 'SID005', 'sdfsd', 'csfsd@dfsfks.com', '8569710025', '92228410fc8b872914e023160cf4ae8f', 0, '2017-07-11 15:41:27', '2017-07-15 17:43:03'),
-(8, 'SID009', 'test', 'test@gmail.com', '2359874527', 'f925916e2754e5e03f75dd58a5733251', 1, '2017-07-11 15:58:28', '2017-07-15 13:42:44'),
-(9, 'SID010', 'Amit', 'amit@gmail.com', '8585856224', 'f925916e2754e5e03f75dd58a5733251', 1, '2017-07-15 13:40:30', NULL),
-(10, 'SID011', 'Sarita Pandey', 'sarita@gmail.com', '4672423754', 'f925916e2754e5e03f75dd58a5733251', 1, '2017-07-15 18:00:59', NULL);
+(1, 'SID002', 'Anuj kumar', 'khanhvinh1@gmail.com', '9865472555', '123', 1, '2017-07-11 15:37:05', '2019-12-21 18:27:42'),
+(4, 'SID005', 'sdfsd', 'khanh@vinh.com', '8569710025', '123', 0, '2017-07-11 15:41:27', '2019-12-21 18:41:22'),
+(8, 'SID009', 'test', 'test@gmail.com', '2359874527', '123', 1, '2017-07-11 15:58:28', '2019-12-21 18:28:12'),
+(9, 'SID010', 'Amit', 'thanhtuan@gmail.com', '8585856224', '123', 1, '2017-07-15 13:40:30', NULL),
+(10, 'SID011', 'Sarita Pandey', 'quocviet@gmail.com', '4672423754', '123', 1, '2017-07-15 18:00:59', NULL),
+(11, '3', 'Luong Dinh Khanh Vinh', 'khanhvinh169@gmail.com', '0123456789', '202cb962ac59075b964b07152d234b70', 1, '2019-12-21 18:35:24', NULL);
 
 --
--- Indexes for dumped tables
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `admin`
+-- Chỉ mục cho bảng `admin`
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tblauthors`
+-- Chỉ mục cho bảng `tblauthors`
 --
 ALTER TABLE `tblauthors`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tblbooks`
+-- Chỉ mục cho bảng `tblbooks`
 --
 ALTER TABLE `tblbooks`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tblcategory`
+-- Chỉ mục cho bảng `tblcategory`
 --
 ALTER TABLE `tblcategory`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tblissuedbookdetails`
+-- Chỉ mục cho bảng `tblissuedbookdetails`
 --
 ALTER TABLE `tblissuedbookdetails`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tblstudents`
+-- Chỉ mục cho bảng `tblstudents`
 --
 ALTER TABLE `tblstudents`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `StudentId` (`StudentId`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `admin`
+-- AUTO_INCREMENT cho bảng `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
--- AUTO_INCREMENT for table `tblauthors`
+-- AUTO_INCREMENT cho bảng `tblauthors`
 --
 ALTER TABLE `tblauthors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
 --
--- AUTO_INCREMENT for table `tblbooks`
+-- AUTO_INCREMENT cho bảng `tblbooks`
 --
 ALTER TABLE `tblbooks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
--- AUTO_INCREMENT for table `tblcategory`
+-- AUTO_INCREMENT cho bảng `tblcategory`
 --
 ALTER TABLE `tblcategory`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
 --
--- AUTO_INCREMENT for table `tblissuedbookdetails`
+-- AUTO_INCREMENT cho bảng `tblissuedbookdetails`
 --
 ALTER TABLE `tblissuedbookdetails`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
 --
--- AUTO_INCREMENT for table `tblstudents`
+-- AUTO_INCREMENT cho bảng `tblstudents`
 --
 ALTER TABLE `tblstudents`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
